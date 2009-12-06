597fcc29428218517154f5207da0aadf51d2add5
k1LoW <k1lowxb@gmail.com>
2009-12-07 02:43:56 +0900

    temp commit

--- ac-cake.el
+++ ac-cake.el
@@ -2,36 +2,48 @@
 ;; -*- Mode: Emacs-Lisp -*-
 (require 'auto-complete)
 
-(defun ac-cake-candidate ()
-  (let ((ac-cake-index nil))
-    (unless (not
-             (and (cake-set-app-path) (executable-find "grep")))
-      (with-temp-buffer (anything-candidate-buffer 'local)
-                        (call-process-shell-command
-                         (concat "grep '[^_]function' "
-                                 cake-app-path
-                                 "models/*.php --with-filename")
-                         nil (current-buffer))
-                        (goto-char (point-min))
-                        (while (not (eobp))
-                          (if (not (re-search-forward ".+\\/\\(.+\\)\.php:.*function *\\([^ ]+\\) *(.*).*" nil t))
-                              (goto-char (point-max))
-                            (setq cp (match-beginning 0))
-                            (setq function-name (match-string 2))
-                            (setq cake-singular-name (match-string 1))
-                            (setq class-name (cake-camelize cake-singular-name))
-                            (goto-char cp)
-                            (delete-region (point) (save-excursion (end-of-line) (point)))
-                            (push (concat class-name "->" function-name) ac-cake-index)
-                            ))))
-    ac-cake-index))
+(defvar ac-cake-index nil)
 
+(defun ac-build-cake-index ()
+  (setq ac-cake-index nil)
+  (unless (not
+           (and (cake-set-app-path) (executable-find "grep")))
+    (with-temp-buffer (anything-candidate-buffer 'local)
+                      (call-process-shell-command
+                       (concat "grep '[^_]function' "
+                               cake-app-path
+                               "models/*.php --with-filename")
+                       nil (current-buffer))
+                      (goto-char (point-min))
+                      (while (not (eobp))
+                        (if (not (re-search-forward ".+\\/\\(.+\\)\.php:.*function *\\([^ ]+\\) *(.*).*" nil t))
+                            (goto-char (point-max))
+                          (setq cp (match-beginning 0))
+                          (setq function-name (match-string 2))
+                          (setq cake-singular-name (match-string 1))
+                          (setq class-name (cake-camelize cake-singular-name))
+                          (goto-char cp)
+                          (delete-region (point) (save-excursion (end-of-line) (point)))
+                          (push (concat class-name "->" function-name) ac-cake-index)
+                          )))
+    ))
+
+(defun ac-cake-trigger ()
+  (if (not ac-prefix)
+      nil
+    (progn
+      ;;(message "%s" (substring ac-point (- ac-point 2)))
+      (if (eq "->" (substring ac-point (- ac-point 2)))
+          t
+        nil)
+      )))
+;;(ac-cake-trigger)
 ;;(ac-cake-candidate)
 
 (defvar ac-source-cake
-  '((candidates
-     . ac-cake-candidate)
-    (cache))
+  '((init . (lambda() (ac-build-cake-index)))
+    (candidates . ac-cake-index)
+    )
   "Source for CakePHP")
 ;;(setq ac-sources '(ac-source-cake))
 
