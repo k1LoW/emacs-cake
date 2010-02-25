d704b17f9178ce3e1cd86a522893322150325d40
k1LoW <k1lowxb@gmail.com>
2010-02-25 23:22:13 +0900

    Change version.

--- cake.el
+++ cake.el
@@ -17,7 +17,7 @@
 ;; along with this program; if not, write to the Free Software
 ;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
-;; Version: 1.2.3
+;; Version: 1.2.4a
 ;; Author: k1LoW (Kenichirou Oyama), <k1lowxb [at] gmail [dot] com> <k1low [at] 101000lab [dot] org>
 ;; URL: http://code.101000lab.org, http://trac.codecheck.in
 
